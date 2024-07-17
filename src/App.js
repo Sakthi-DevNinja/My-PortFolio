ad3edfe655e8b6e0949504c9828051c66e1813a3
Sakthi-DevNinja <147724668+Sakthi-DevNinja@users.noreply.github.com>
2024-07-17 20:42:24 +0530

    particless added
    success

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 // import Footer from './Footer';
 import Landing from './Landing';
 import './App.css';
+import ParticlesComponent from './components/particles';
+
 
 function App() {
   return (
     <div className="App">
+      <ParticlesComponent id="particles" />
       {/* <Header /> */}
       <Landing />
       {/* <Footer /> */}
