efb155b888a871704f3902fbc2fca6c8a340a5a1
Sakthi-DevNinja <147724668+Sakthi-DevNinja@users.noreply.github.com>
2024-07-17 23:30:02 +0530

    fav icon updated

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -32,29 +32,17 @@ const Landing = () => {
             <div className='about-me'>Enthusiastic about crafting web solutions <br /> HTML, CSS, JavaScript, PHP, MySQL</div>
 
             <div className='contact-list'>
-                <a href='https://wa.me/9585919130?text=Welcome!' target='_BLANK' rel="noreferrer" className="social-links whatsapp">
-                    <FontAwesomeIcon icon={faWhatsapp} />
-                </a>
+                <a href='https://wa.me/9585919130?text=Welcome!' target='_BLANK' rel="noreferrer" className="social-links whatsapp"><FontAwesomeIcon icon={faWhatsapp} /></a>
                 
-                <a href='https://www.facebook.com/profile.php?id=100011062291623&mibextid=ZbWKwL' target='_BLANK' rel="noreferrer" className="social-links facebook">
-                    <FontAwesomeIcon icon={faFacebookF} />
-                </a>
+                <a href='https://www.facebook.com/profile.php?id=100011062291623&mibextid=ZbWKwL' target='_BLANK' rel="noreferrer" className="social-links facebook"><FontAwesomeIcon icon={faFacebookF} /></a>
 
-                <a href='https://www.instagram.com/sakthivel14325?igsh=b2JrcWtsZHc3ZnUw' target='_BLANK' rel="noreferrer" className="social-links instagram">
-                    <FontAwesomeIcon icon={faInstagram} />
-                </a>
+                <a href='https://www.instagram.com/sakthivel14325?igsh=b2JrcWtsZHc3ZnUw' target='_BLANK' rel="noreferrer" className="social-links instagram"><FontAwesomeIcon icon={faInstagram} /></a>
 
-                <a href='https://www.linkedin.com/in/sakthivel-dev' target='_BLANK' rel="noreferrer" className="social-links linkedin">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-                </a>
+                <a href='https://www.linkedin.com/in/sakthivel-dev' target='_BLANK' rel="noreferrer" className="social-links linkedin"><FontAwesomeIcon icon={faLinkedinIn} /></a>
 
-                <a href='#' target='_BLANK' rel="noreferrer" className="social-links twitterx">
-                    <FontAwesomeIcon icon={faXTwitter} />
-                </a> 
+                <a href='#' target='_BLANK' rel="noreferrer" className="social-links twitterx"><FontAwesomeIcon icon={faXTwitter} /></a> 
 
-                <a href='https://github.com/Sakthi-DevNinja' target='_BLANK' rel="noreferrer" className="social-links github">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>  
+                <a href='https://github.com/Sakthi-DevNinja' target='_BLANK' rel="noreferrer" className="social-links github"><FontAwesomeIcon icon={faGithub} /></a>  
             </div>
 
         </div>
