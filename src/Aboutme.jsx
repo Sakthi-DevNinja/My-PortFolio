3737708ba5357ef6ac8a822bdf3bcf412d6dec95
Sakthi-DevNinja <147724668+Sakthi-DevNinja@users.noreply.github.com>
2024-07-18 00:29:49 +0530

    fav icon updated

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -2,12 +2,39 @@ import React from 'react'
 
 const Aboutme = () => {
   return (
-    <div id='aboutme'>
-        <div className="sec-hdr">
-            <h1 className="class-hdr">About Me</h1>
+    <div id='aboutme' >
+
+        <div className='content-blk'>
+            <div className="sec-hdr">
+                <h1 className="class-hdr">Contact</h1>
+            </div>
+            <div className="content-dtl">Conatac details here...</div>
         </div>
 
+        <div className='content-blk'>
+            <div className="sec-hdr">
+                <h1 className="class-hdr">Objective</h1>
+            </div>
+            <div className="content-dtl">I'm a detail-oriented Full Stack Web Developer skilled in creating and deploying dynamic web applications. Seeking to apply my expertise in both front-end and back-end development at an innovative tech company. Dedicated to contributing to collaborative teams and advancing cutting-edge projects.</div>
+        </div>
+
+        <div className='content-blk'>
+            <div className="sec-hdr">
+                <h1 className="class-hdr">Skills</h1>
+            </div>
+            <div className="content-dtl">Skills Here...</div>
+        </div>
+
+        <div className='content-blk'>
+            <div className="sec-hdr">
+                <h1 className="class-hdr">Education</h1>
+            </div>
+            <div className="content-dtl">Acadamic infromations here...</div>
+        </div>
+
+
     </div>
+    
   )
 }
 
