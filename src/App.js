664d0851f9460bc9ec56cbad287d5e144d93fd01
Sakthi-DevNinja <147724668+Sakthi-DevNinja@users.noreply.github.com>
2024-07-17 22:51:45 +0530

    fav icon updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import ParticlesComponent from './components/particles';
 
 
+
 function App() {
   return (
     <div className="App">
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import Image from './images/sv.jpeg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faWhatsapp,
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons';
+
+
  
 const Landing = () => {
   return (
@@ -18,6 +30,33 @@ const Landing = () => {
                 <div className="role">FULL STACK DEVELOPER</div>
             </div>
             <div className='about-me'>Enthusiastic about crafting web solutions <br /> HTML, CSS, JavaScript, PHP, MySQL</div>
+
+            <div className='contact-list'>
+                <div className="social-links whatsapp">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                </div>
+                
+                <div className="social-links facebook">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                </div>
+
+                <div className="social-links instagram">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </div>
+
+                <div className="social-links linkedin">
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                </div>
+
+                <div className="social-links twitterx">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </div>
+
+                <div className="social-links github">
+                    <FontAwesomeIcon icon={faGithub} />
+                </div>  
+            </div>
+
         </div>
     </main>
   )
