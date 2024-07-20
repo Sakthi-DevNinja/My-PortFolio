7f4012902ea9dcdbd8da255e3cd24c4540a441c2
Sakthi-DevNinja <147724668+Sakthi-DevNinja@users.noreply.github.com>
2024-07-21 00:52:39 +0530

    microsoft clarity

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ParticlesComponent from './components/particles';
 import { useTheme } from './context/ThemeContext';
 
 import './styles/global.css'; // Import global styles
+import { Helmet } from 'react-helmet';
 
 
 
@@ -26,6 +27,18 @@ function App() {
 
   return (
     <div  className={`App ${theme}`}>
+
+    <Helmet>
+      <script type="text/javascript">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");
+        `}
+      </script>
+    </Helmet>
        
       <ParticlesComponent id="particles" />
       <Header />
