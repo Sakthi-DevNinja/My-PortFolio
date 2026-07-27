0b6ed91743dcf4efbfde98f802d0a2e7c60cd498
Sakthivel S <147724668+Sakthi-DevNinja@users.noreply.github.com>
2026-07-27 11:00:32 +0530

    Update SocialMedia.jsx

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp, faFacebookF, faInstagram, faLinkedinIn, faXTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp, faFacebookF, faInstagram, faLinkedinIn, faXTwitter} from '@fortawesome/free-brands-svg-icons';
 
 
 const SocialMedia = () => {
